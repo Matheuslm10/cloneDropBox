f3ffe4acdd180d1b7af9edb2dbc28916f030e887
Matheus Machado <matheus_lm10@hotmail.com>
2019-04-11 20:10:30 -0400

    criado dois models

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://matheus:<omnistack>@cluster0-nx06r.mongodb.net/omnidatabase?retryWrites=true", 
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(express.json()); //esse modulo, que tb eh um midleware, permite o servidor entender as requisicoes em formato json 
 app.use(express.urlencoded({ extended: true })); //permite que seja enviado arquivos nas requisicoes
 
 app.use(require('./routes'));
 
-app.listen(3333);
+app.listen(3332);
 
 
